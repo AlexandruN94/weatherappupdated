60b2f8fe9bc925a2cd54b8d1b9050456db13a615
Alexandru Nagy <nagy.alexandru94@gmail.com>
2023-04-11 19:33:42 +0300

    Refactored, styled and added the final touches to the project

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const textInputField = document.getElementById('city');
 const getWeatherBtn = document.querySelector('.weatherbtn');
-// const nav = document.querySelector('.nav');
 
 const weekDaysDictionary = {
   0: 'Sunday',
@@ -13,6 +12,8 @@ const weekDaysDictionary = {
 };
 
 const renderCityData = function (data) {
+  // City container
+
   const cityContainer = document.createElement('div');
   cityContainer.classList.add('cityContainer');
 
@@ -35,17 +36,25 @@ const renderCityData = function (data) {
   const degrees = document.createElement('p');
   degrees.innerHTML = `${data.current.temp_c}°C`;
   degreesDiv.appendChild(degrees);
-
   cityContainer.appendChild(degreesDiv);
 
   const textTag = document.createElement('p');
   textTag.innerHTML = data.current.condition.text;
   cityContainer.appendChild(textTag);
 
+  const cityDataContainer = document.createElement('div');
+  cityDataContainer.classList.add('citydatacontainer');
+  cityDataContainer.appendChild(cityName);
+  cityDataContainer.appendChild(currentDate);
+  cityDataContainer.appendChild(degreesDiv);
+  cityDataContainer.appendChild(textTag);
+  cityContainer.appendChild(cityDataContainer);
+
+  // Forecast container
+
   const forecastTitle = document.createElement('p');
   const dayWord = data.forecast.forecastday.length === 1 ? `day` : `days`;
-
-  forecastTitle.innerHTML = `The forecast for the following ${data.forecast.forecastday.length} ${dayWord}:`;
+  forecastTitle.innerHTML = `The forecast for the following ${data.forecast.forecastday.length} ${dayWord} in ${data.location.name} is:`;
 
   const forecast = document.createElement('div');
   forecast.appendChild(forecastTitle);
@@ -56,51 +65,30 @@ const renderCityData = function (data) {
       weekDaysDictionary[new Date(el.date).getDay()]
     }: ${el.day.maxtemp_c}°C`;
     forecast.appendChild(dailyForecast);
-
-    // forecast.classList.add('forecastContainer');
   });
-
-  // `The weather in ${data.location.name} for ${
-  //   weekDaysDictionary[new Date(el.date).getDay() + 1]
-  // } is ${el.day.maxtemp_c}°C.`;
-
-  // Weather images
-  if (data.current.is_day == 0) {
-    cityContainer.classList.add('nightmode');
-  } else {
-    if (data.current.condition.text == 'Partly cloudy') {
-      cityContainer.classList.add('partlycloudy');
-    } else if (data.current.condition.text == 'Sunny' || 'Clear') {
-      cityContainer.classList.add('sunnyclear');
-    } else if (data.current.condition.text == 'Cloudy') {
-      cityContainer.classList.add('cloudy');
-    } else if (data.current.condition.text == 'Overcast') {
-      cityContainer.classList.add('overcast');
-    } else if (data.current.condition.text == 'Mist') {
-      cityContainer.classList.add('mist');
-    } else if (data.current.condition.text == 'Patchy rain possible') {
-      cityContainer.classList.add('patchyrainpossible');
-    }
-  }
 
   cityContainer.appendChild(forecast);
 
-  const leftSide = document.createElement('div');
-  leftSide.appendChild(cityName);
-  leftSide.appendChild(currentDate);
-  leftSide.appendChild(degreesDiv);
-  leftSide.appendChild(textTag);
-  cityContainer.appendChild(leftSide);
-  leftSide.classList.add('leftSide');
+  const forecastDataContainer = document.createElement('div');
+  forecastDataContainer.classList.add('forecastdatacontainer');
+  forecastDataContainer.appendChild(forecast);
+  cityContainer.appendChild(forecastDataContainer);
 
-  const rightSide = document.createElement('div');
+  // Weather images
 
-  rightSide.appendChild(forecast);
-  cityContainer.appendChild(rightSide);
-  rightSide.classList.add('rightSide');
+  if (data.current.is_day == 0) {
+    cityContainer.classList.add('night');
+  } else {
+    cityContainer.classList.add(
+      data.current.condition.text.replaceAll(' ', '').toLowerCase()
+    );
+  }
+  cityContainer.classList.add('bg-cover');
 
   return cityContainer;
 };
+
+// Weather API fetch
 
 const getWeatherForCity = (city, days) => {
   const encodedCity = encodeURIComponent(city);
@@ -114,14 +102,10 @@ const getWeatherForCity = (city, days) => {
     .then(data => {
       const cityData = document.getElementById('citydata');
       cityData.appendChild(renderCityData(data));
-      console.log(data);
-      // if (data.current.is_day == 1) {
-      //   document.body.style.background = 'orange';
-      // } else {
-      //   document.body.style.background = 'black';
-      // }
     });
 };
+
+// Event listeners
 
 const triggerWeatherFetch = function () {
   const city = document.getElementById('city').value;
@@ -130,7 +114,6 @@ const triggerWeatherFetch = function () {
 };
 
 window.addEventListener('load', e => {
-  console.log(e);
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
       function (position) {
@@ -153,113 +136,3 @@ textInputField.addEventListener('keydown', function (e) {
 getWeatherBtn.addEventListener('click', function () {
   triggerWeatherFetch();
 });
-
-///////////////////////// Backup code /////////////////////////////
-
-/* const textInputField = document.getElementById('city');
-const getWeatherBtn = document.querySelector('.weatherbtn');
-
-const weekDaysDictionary = {
-  1: 'Monday',
-  2: 'Tuesday',
-  3: 'Wednesday',
-  4: 'Thursday',
-  5: 'Friday',
-  6: 'Saturday',
-  0: 'Sunday',
-};
-
-const renderCityData = function (data) {
-  const cityContainer = document.createElement('div');
-  cityContainer.classList.add('cityContainer');
-
-  const cityName = document.createElement('h2');
-  cityName.innerHTML = data.location.name;
-  cityName.classList.add('cityName');
-  cityContainer.appendChild(cityName);
-
-  const currentDate = document.createElement('p');
-  currentDate.innerHTML = weekDaysDictionary[new Date().getDay()];
-  cityContainer.appendChild(currentDate);
-
-  const degreesDiv = document.createElement('div');
-  degreesDiv.classList.add('degreesContainer');
-
-  let image = document.createElement('img');
-  image.setAttribute('src', data.current.condition.icon);
-  degreesDiv.appendChild(image);
-
-  const degrees = document.createElement('p');
-  degrees.innerHTML = `${data.current.temp_c}°C`;
-  degreesDiv.appendChild(degrees);
-
-  cityContainer.appendChild(degreesDiv);
-
-  const textTag = document.createElement('p');
-  textTag.innerHTML = data.current.condition.text;
-  cityContainer.appendChild(textTag);
-
-  const forecast = document.createElement('div');
-  data.forecast.forecastday.forEach(el => {
-    const dailyForecast = document.createElement('p');
-    dailyForecast.innerHTML = `The weather in ${data.location.name} for ${
-      weekDaysDictionary[new Date(el.date).getDay() + 1]
-    } is ${el.day.maxtemp_c}°C.`;
-    forecast.appendChild(dailyForecast);
-  });
-
-  cityContainer.appendChild(forecast);
-  return cityContainer;
-};
-
-const getWeatherForCity = city => {
-  const encodedCity = encodeURIComponent(city);
-
-  fetch(
-    `https://api.weatherapi.com/v1/forecast.json?key=ed104a87c20f401d9ea135033230504&q=${encodedCity}&days=6`
-  )
-    .then(res => {
-      return res.json();
-    })
-    .then(data => {
-      const cityData = document.getElementById('citydata');
-      cityData.appendChild(renderCityData(data));
-
-      // const forecastDiv = document.getElementById('forecast');
-      // data.forecast.forecastday.forEach(el => {
-      //   const d = document.createElement('div');
-      //   d.innerHTML = `The weather in ${city} for ${
-      //     weekDays[new Date(el.date).getDay() + 1]
-      //   } is ${el.day.maxtemp_c}°C.`;
-      //   forecast.appendChild(d);
-      //   console.log(el);
-      // });
-    });
-};
-
-window.addEventListener('load', e => {
-  console.log(e);
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      function (position) {
-        const { latitude, longitude } = position.coords;
-        getWeatherForCity(`${latitude},${longitude}`);
-      },
-      function () {
-        alert('Could not retrieve your location.');
-      }
-    );
-  }
-});
-
-textInputField.addEventListener('keydown', function (e) {
-  if (e.key === 'Enter') {
-    const value = document.getElementById('city').value;
-    getWeatherForCity(value);
-  }
-});
-
-getWeatherBtn.addEventListener('click', function () {
-  const value = document.getElementById('city').value;
-  getWeatherForCity(value);
-}); */
